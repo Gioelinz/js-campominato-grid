0ec574c4f237084233301906b63014fc4836ee7d
Gioelinz <gioelerunza.gr@gmail.com>
2022-01-17 16:12:24 +0100

    add js functions for difficulty

--- js/script.js
+++ js/script.js
@@ -5,3 +5,27 @@ con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro. */
+
+// ! funzione per difficoltà
+function Difficulty(cells, columns) {
+    const Cells = cells;
+    const Columns = columns;
+    const TotalCells = Cells + Columns;
+
+    return TotalCells;
+}
+
+
+// ! creazione celle
+
+function createCells() {
+    const cell = document.createElement("div");
+    cell.className = 'cell';
+
+    return cell
+}
+// ! inizializzo variabili che mi servono per il DOM
+
+const difficultyElement = document.getElementById("difficulty");
+const buttonElement = document.getElementById("button");
+const gridElement = document.getElementById("grid");
